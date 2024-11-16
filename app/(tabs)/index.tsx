2c448584a46b5555bc4867a20889816bf29ed3ae
Nwosu Chukwuemeka Darlington <fritzdultimate@gmail.com>
2024-11-16 19:28:00 +0100

    init

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,39 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
+import { Image } from 'expo-image';
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
